3ff626d2eed22c14747e6341487f61bed84d4edd
Matias Rozanec <rozanecm@gmail.com>
2018-03-22 19:36:35 -0300

    file output

--- teams_preferences_generator.py
+++ teams_preferences_generator.py
@@ -1,12 +1,23 @@
+# NOTE: this script works with teams and players counting from 0. This should be corrected.
 from numpy import random 
+
 NUM_OF_PLAYERS = 200
 NUM_OF_TEAMS = 20
 VACANCIES_PER_TEAM = 10
 
 preferences = []
 
+# create an array of random numbers from 0 to NUM_OF_PLAYERS for each team.
 for i in range(0,NUM_OF_TEAMS):
     preferences.append(random.choice(NUM_OF_PLAYERS, size=NUM_OF_PLAYERS, replace=False))
 
+
+# save data to files
+base_filename = "equipo_"
+
 for i in range(0, NUM_OF_TEAMS):
-    print(i, preferences[i])
+    current_filename = base_filename + str(i) + ".prf"
+    f = open(current_filename, 'w')
+    for j in range(NUM_OF_PLAYERS):
+        f.write(str(preferences[i][j]) + '\n')
+    f.close()
