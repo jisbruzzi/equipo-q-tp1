3fb2a40261153513bc9c6ff702ec277b39649ec2
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-24 15:34:45 -0300

    Unifico código de los generadores

--- src/gale_shapely/players_preferences_generator.py
+++ src/gale_shapely/players_preferences_generator.py
@@ -1,22 +1,10 @@
-from numpy import random
+# NOTE: this script works with teams and players counting from 0. This should be corrected.
+
+from src.gale_shapely.generator import generate
 
 NUM_OF_PLAYERS = 200
 NUM_OF_TEAMS = 20
-VACANCIES_PER_TEAM = 10
 
-preferences = []
-
-# create an array of random numbers from 0 to NUM_OF_PLAYERS for each team.
-for i in range(NUM_OF_PLAYERS):
-    preferences.append(random.choice(NUM_OF_TEAMS, size=NUM_OF_TEAMS, replace=False) + 1)
-
-
-# save data to files
-base_filename = "jugador_"
-
-for i in range(NUM_OF_PLAYERS):
-    current_filename = base_filename + str(i + 1) + ".prf"
-    f = open(current_filename, 'w')
-    for j in range(NUM_OF_TEAMS):
-        f.write(str(preferences[i][j]) + '\n')
-    f.close()
+if __name__ == '__main__':
+    teams = [team for team in range(1, NUM_OF_TEAMS + 1)]
+    generate(teams, NUM_OF_PLAYERS, 'jugador')
--- src/gale_shapely/teams_preferences_generator.py
+++ src/gale_shapely/teams_preferences_generator.py
@@ -1,23 +1,9 @@
 # NOTE: this script works with teams and players counting from 0. This should be corrected.
-from numpy import random
+from src.gale_shapely.generator import generate
 
 NUM_OF_PLAYERS = 200
 NUM_OF_TEAMS = 20
-VACANCIES_PER_TEAM = 10
 
-preferences = []
-
-# create an array of random numbers from 0 to NUM_OF_PLAYERS for each team.
-for i in range(NUM_OF_TEAMS):
-    preferences.append(random.choice(NUM_OF_PLAYERS, size=NUM_OF_PLAYERS, replace=False) + 1)
-
-
-# save data to files
-base_filename = "equipo_"
-
-for i in range(NUM_OF_TEAMS):
-    current_filename = base_filename + str(i + 1) + ".prf"
-    f = open(current_filename, 'w')
-    for j in range(NUM_OF_PLAYERS):
-        f.write(str(preferences[i][j]) + '\n')
-    f.close()
+if __name__ == '__main__':
+    players = [player for player in range(1, NUM_OF_PLAYERS + 1)]
+    generate(players, NUM_OF_TEAMS, 'equipo')
