001552e987fb53adc79d6b4612910f127a846242
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-24 23:19:47 -0300

    Correcciones a la verificación de inestabilidades

--- test/gale_shapely_test.py
+++ test/gale_shapely_test.py
@@ -59,18 +59,37 @@ class GSTest(unittest.TestCase):
 
         self.run_gs(p, t)
 
+    def test_simple_case(self):
+        p = {0: [0, 1], 1: [0, 1], 2: [0, 1], 3: [1, 0]}
+        t = {0: [3, 1, 0, 2], 1: [1, 2, 3, 0]}
+        self.run_gs(p, t)
+
     def run_gs(self, p, t):
         matches = gale_shapely(t, p)
+        for key in p:
+            p[key] = {item: i for i, item in enumerate(p[key])}
+
+        for key in t:
+            t[key] = {item: i for i, item in enumerate(t[key])}
+
         self.assertNotEqual(len(matches), 0)
         for team, player in matches:
-            for other_team in p[player][:p[player].index(team)]:
-                other_team_matches = filter(lambda x: x[0] == other_team, matches)
+            for other_team in p[player].keys():
+                if other_team == team:
+                    continue
                 # Comparo los matches (team, player) con (other_team, other_player)
                 # Hay inestabilidad si team prefiere a other_player antes que a player,
                 # y other_team prefiere a player antes que a other_player
-                for _, other_player in other_team_matches:
-                    check = \
-                        t[other_team].index(player) < t[other_team].index(other_player) and \
-                        t[team].index(player) > t[team].index(other_player)
-                    self.assertFalse(check,
-                                     "Inestabilidad: {} {}".format((team, player), (other_team, other_player)))
+                for _team, other_player in matches:
+                    if _team != other_team:
+                        continue
+
+                    player_prefers_other_team = p[player][other_team] < p[player][team]
+                    other_team_prefers_player = t[other_team][player] < t[other_team][other_player]
+
+                    team_prefers_other_player = t[team][other_player] < t[team][player]
+                    other_player_prefers_team = p[other_player][team] < p[other_player][other_team]
+
+
+                    self.assertFalse((player_prefers_other_team and other_team_prefers_player) or
+                                     (team_prefers_other_player and other_player_prefers_team))
