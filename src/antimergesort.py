34e0d27141f0e1a3d732dba38c8b44c9d2abfba8
jisbruzzi <jose.sbru@gmail.com>
2018-03-25 18:15:00 -0300

    Agrego algoritmo de antimergesort

---
+++ src/antimergesort.py
@@ -0,0 +1,38 @@
+import math
+import mergesort
+
+
+def antimerge(ordenada):
+    if len(ordenada) <= 1:
+        return ordenada
+    ordenadaSinUltimo = list(ordenada)
+    ultimo = ordenadaSinUltimo.pop()
+
+    indice_medio = int(math.floor(len(ordenadaSinUltimo) / 2))
+    l1 = ordenadaSinUltimo[0:indice_medio] + [ultimo]
+    l2 = ordenadaSinUltimo[indice_medio:]
+
+    return l2, l1
+
+
+def antimergesort(ordenada):
+    if len(ordenada) <= 1:
+        return ordenada
+
+    l1, l2 = antimerge(ordenada)
+    return antimergesort(l1) + antimergesort(l2)
+
+
+def prueba(a):
+    q = antimergesort(a)
+    print(q)
+    print(mergesort.ordenar(q))
+
+
+prueba([1, 2, 3])
+print("********")
+prueba([1, 2, 3, 4])
+print("********")
+prueba([1, 2, 3, 4, 5, 6])
+print("********")
+prueba([1, 2, 3, 4, 5, 6, 7, 8])
