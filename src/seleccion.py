0b0f2deaea393cc0d9ad680500b1ea71e7a39557
juanchoxl2231 <37672569+juanchoxl2231@users.noreply.github.com>
2018-03-26 12:41:49 -0300

    Add files via upload

---
+++ src/seleccion.py
@@ -0,0 +1,16 @@
+def swap(lista indice indiceminimo)
+aux=lista[indice]
+lista[indice]=lista[indiceminimo]
+lista[indiceminimo]=aux
+
+def seleccion(lista) #este es de orden O(n2)
+i=0
+j=0
+for i in range[0,len(lista)-1] #comienza desde el 0 hasta el largo de la lista menos uno
+	min=i
+	for j in range [i+1, len(lista)] #este for va del segundo hasta el untimo inclusive
+		if (lista[j]<lista[min]) #busca el menor entre todos
+			min=j #aca tengo el subindice del menor de todos
+		swap(lista,i,min)#intercambiar, recordar que comienza de 0
+		j=j+1
+i=i+1
