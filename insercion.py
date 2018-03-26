499f4e16389bba36f491e9c7db8fa9cfc0eb06d5
juanchoxl2231 <37672569+juanchoxl2231@users.noreply.github.com>
2018-03-26 12:40:01 -0300

    Add files via upload
    agrego los métodos de ordenamiento inserción, quicksort y selecciòn

---
+++ insercion.py
@@ -0,0 +1,12 @@
+def insercion(lista):
+	i=0
+	j=0
+	auxiliar=0
+	while (i< (len(lista)-1) #recorro la lista de punta a punta
+		j=i
+		while ((j>0) && (lista[i]<lista [j-1])) #aca llego la hora de intercambiar donde cada operacion de cambio de valor es O(1)
+			auxiliar=lista[j]
+			lista[j]=lista[j-1]
+			lista[j-1]=auxiliar
+			j--
+return lista		
---
+++ quicksort.py
@@ -0,0 +1,23 @@
+def swap (lista indice1 indice2)
+	aux=lista[indice1]
+	lista[indice1]=lista[indice2]
+	lista[indice2]=aux
+
+def quicksort(lista inf sup): #cuando se lo llama se lo invoca con inf=0 y sup=largo de la lista
+if (inf<sup)
+	elem_div=lista[sup]
+	aux
+	i=inf-1
+	j=sup
+	cont=1
+	while cont
+		while (lista[i++]<elem_div)
+			while (lista[j--]>elem_div)
+				if (i<j)
+					swap (lista i j)
+				else
+					cont=0 #actua como rompedor del while ya que los indices se cruzan
+	swap (lista i sup)
+	quicksort(lista inf i-1)
+	quicksort(lista i+1 sup)
+return lista
