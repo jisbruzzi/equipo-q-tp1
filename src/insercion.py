95c29eb42c0b99f5615add86424d1e8f715b81a3
jisbruzzi <jose.sbru@gmail.com>
2018-04-07 18:39:07 -0300

    Ahora si que es insercion

--- src/insercion.py
+++ src/insercion.py
@@ -1,15 +1,21 @@
 def insertar(elemento,lista):
-	menor=filter(lambda x: x <= elemento,lista)
-	mayor=filter(lambda x: x >  elemento,lista)
-	return menor + [elemento] + mayor
+	lista = list(lista)
+	resultado = []
+	while len(lista) > 0:
+		e = lista.pop(0)
+		if e < elemento:
+			resultado = resultado + [e]
+		else:
+			lista=[e]+lista
+			break
+	return resultado+[elemento]+lista
 
 def insercion(lista):
-	lista=list(lista)#copia
-	resultado=[]
+	lista = list(lista)#copia
+	resultado = []
 	for e in lista:
-		resultado=insertar(e,resultado)
+		resultado = insertar(e, resultado)
 	return resultado
-
 
 def ordenar(lista):
 	return insercion(lista)
