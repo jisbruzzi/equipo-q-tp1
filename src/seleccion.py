cfdba7f350907cf07827fd072330aa21a1535484
Matias Rozanec <rozanecm@gmail.com>
2018-04-08 01:40:35 -0300

    correct format of selection alg.

--- src/seleccion.py
+++ src/seleccion.py
@@ -1,20 +1,16 @@
 def swap(lista, indice, indiceminimo):
-    aux=lista[indice]
-    lista[indice]=lista[indiceminimo]
-    lista[indiceminimo]=aux
+    aux = lista[indice]
+    lista[indice] = lista[indiceminimo]
+    lista[indiceminimo] = aux
 
 def ordenar(lista): #este es de orden O(n2)
-    #i=0
-    #j=0
     for i in range(len(lista)-1): #comienza desde el 0 hasta el largo de la lista menos uno
-        min=i
+        min = i
         for j in range(i+1, len(lista)): #este for va del segundo hasta el untimo inclusive
-            if lista[j]<lista[min]: #busca el menor entre todos
-                min=j #aca tengo el subindice del menor de todos
+            if lista[j] < lista[min]: #busca el menor entre todos
+                min = j #aca tengo el subindice del menor de todos
         if min != i:
-            swap(lista,i,min)#intercambiar, recordar que comienza de 0
-    #j=j+1
-    #i=i+1
+            swap(lista, i, min) #intercambiar, recordar que comienza de 0
     return lista
 
 print(ordenar([3,2,1]))
